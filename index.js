9eb343d22cc6b0f566e730362dc2da4d25302332
unknown <tuncer.fm@gmail.com>
2023-05-09 18:56:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+  
+
+app.post('/count-words', (req, res) => {
+  const inputText = req.body.text;
+  const wordCount = inputText.split(' ').length;
+  res.send(`Text contains ${wordCount} words.`);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
